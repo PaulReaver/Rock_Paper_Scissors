e6fbf85bdc2a8e47b8f7427bcb9e7314b114083a
PaulReaver <35564306+PaulReaver@users.noreply.github.com>
2022-09-30 01:49:01 +0300

    Gets player choice

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //Create array with computer options
 const ARRAY = ["Rock", "Paper", "Scissors"];
 
-//Assign to computer option a choice
-let computerSelection = getComputerChoice(ARRAY);
+//Computer makes a choice
+const computerSelection = getComputerChoice(ARRAY);
+
+//Get player choice
+let playerSelection = prompt("Pick Rock, Paper or Scissors")
+
+//Gets rid of spaces
+playerSelection = playerSelection.trim();
+//Makes the first letter uppercase and the rest lowercase
+playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
 //Function to get random choice for the computer
 function getComputerChoice(array){
