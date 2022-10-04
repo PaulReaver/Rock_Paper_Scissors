57d5fca2f6362a664f8b995cc9ec14d6f30ba76f
PaulReaver <35564306+PaulReaver@users.noreply.github.com>
2022-10-04 12:37:35 +0300

    Replaced return statements with console.log

--- script.js
+++ script.js
@@ -4,30 +4,23 @@ const ARRAY = ["Rock", "Paper", "Scissors"];
 //Player and computer score
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection;
 
 //Player chooses Rock
 document.querySelector(".rock").addEventListener("click", choiceRock);
 function choiceRock() {
-    playerSelection = "Rock";
-    const RESULT = playRound(playerSelection);
-    console.log(RESULT);
+    playRound("Rock");
 }
 
 //Player chooses Paper
 document.querySelector(".paper").addEventListener("click", choicePaper);
 function choicePaper() {
-    playerSelection = "Paper";
-    const RESULT = playRound(playerSelection);
-    console.log(RESULT);
+    playRound("Paper");
 }
 
 //Player chooses Scissors
 document.querySelector(".scissors").addEventListener("click", choiceScissors);
 function choiceScissors() {
-    playerSelection = "Scissors";
-    const RESULT = playRound(playerSelection);
-    console.log(RESULT);
+    playRound("Scissors");
 }
 
 //Function to get random choice for the computer
@@ -43,33 +36,33 @@ function playRound(PLAYERSELECTION){
     if (PLAYERSELECTION === COMPUTERSELECTION){
         playerScore += 1;
         computerScore += 1; 
-        return `It's a tie! You both picked ${PLAYERSELECTION}.`
+        console.log(`It's a tie! You both picked ${PLAYERSELECTION}.`);
     } else {
         if (PLAYERSELECTION === "Rock"){
             if (COMPUTERSELECTION === "Scissors"){
                 playerScore += 1;
-                return "You win! Rock beats Scissors."
+                console.log("You win! Rock beats Scissors.");
             } else {
                 computerScore += 1;
-                return "You lose! Rock loses to Paper."
+                console.log("You lose! Rock loses to Paper.");
             }
         }
         if (PLAYERSELECTION === "Paper"){
             if (COMPUTERSELECTION === "Rock"){
                 playerScore += 1;
-                return "You win! Paper beats Rock."
+                console.log("You win! Paper beats Rock.");
             } else {
                 computerScore += 1;
-                return "You lose! Paper loses to Scissors."
+                console.log("You lose! Paper loses to Scissors.");
             }
         }
         if (PLAYERSELECTION === "Scissors"){
             if (COMPUTERSELECTION === "Paper"){
                 playerScore += 1;
-                return "You win! Scissors beat Paper."
+                console.log("You win! Scissors beat Paper.");
             } else {
                 computerScore += 1;
-                return "You lose! Scissors lose to Rock."
+                console.log("You lose! Scissors lose to Rock.");
             }
         }
     }
