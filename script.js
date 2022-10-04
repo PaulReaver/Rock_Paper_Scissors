c64be940f19e09f7c401c26775210753df9c959e
PaulReaver <35564306+PaulReaver@users.noreply.github.com>
2022-10-04 13:01:17 +0300

    Current score is displayed on screen

--- script.js
+++ script.js
@@ -37,33 +37,33 @@ function getComputerChoice(array){
 function playRound(PLAYERSELECTION){
     const COMPUTERSELECTION = getComputerChoice(ARRAY);
     if (PLAYERSELECTION === COMPUTERSELECTION){ 
-        results.textContent = `It's a tie! You both picked ${PLAYERSELECTION}.`;
+        results.textContent = `It's a tie! You both picked ${PLAYERSELECTION}. Score: ${playerScore} - ${computerScore}`;
     } else {
         if (PLAYERSELECTION === "Rock"){
             if (COMPUTERSELECTION === "Scissors"){
                 playerScore += 1;
-                results.textContent = "You win! Rock beats Scissors.";
+                results.textContent = `You win! Rock beats Scissors. Score: ${playerScore} - ${computerScore}`;
             } else {
                 computerScore += 1;
-                results.textContent = "You lose! Rock loses to Paper.";
+                results.textContent = `You lose! Rock loses to Paper. Score: ${playerScore} - ${computerScore}`;
             }
         }
         if (PLAYERSELECTION === "Paper"){
             if (COMPUTERSELECTION === "Rock"){
                 playerScore += 1;
-                results.textContent = "You win! Paper beats Rock.";
+                results.textContent = `You win! Paper beats Rock. Score: ${playerScore} - ${computerScore}`;
             } else {
                 computerScore += 1;
-                results.textContent = "You lose! Paper loses to Scissors.";
+                results.textContent = `You lose! Paper loses to Scissors. Score: ${playerScore} - ${computerScore}`;
             }
         }
         if (PLAYERSELECTION === "Scissors"){
             if (COMPUTERSELECTION === "Paper"){
                 playerScore += 1;
-                results.textContent = "You win! Scissors beat Paper.";
+                results.textContent = `You win! Scissors beat Paper. Score: ${playerScore} - ${computerScore}`;
             } else {
                 computerScore += 1;
-                results.textContent = "You lose! Scissors lose to Rock.";
+                results.textContent = `You lose! Scissors lose to Rock. Score: ${playerScore} - ${computerScore}`;
             }
         }
     }
