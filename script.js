a373e045d8b9eb5818dc32a13108d493fc919f87
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-12-17 22:47:38 +0200

    Formatted code

--- script.js
+++ script.js
@@ -1,46 +1,28 @@
-//Create array with computer options
-const ARRAY = ["Rock", "Paper", "Scissors"];
+// Create array with computer options
+const ARRAY = ['Rock', 'Paper', 'Scissors'];
 
-//Player and computer score
+// Player and computer score
 let playerScore = 0;
 let computerScore = 0;
 
-//Gets the div for displaying results
-const results = document.querySelector(".results")
+// Gets the div for displaying results
+const results = document.querySelector('.results');
 
-//Player chooses Rock
-document.querySelector(".rock").addEventListener("click", choiceRock);
-function choiceRock() {
-    playRound("Rock");
-}
-
-//Player chooses Paper
-document.querySelector(".paper").addEventListener("click", choicePaper);
-function choicePaper() {
-    playRound("Paper");
-}
-
-//Player chooses Scissors
-document.querySelector(".scissors").addEventListener("click", choiceScissors);
-function choiceScissors() {
-    playRound("Scissors");
-}
-
-//Function to get random choice for the computer
-function getComputerChoice(array){
+// Function to get random choice for the computer
+function getComputerChoice(array) {
     const RANDOMINDEX = Math.floor(Math.random() * array.length);
     const CHOICE = array[RANDOMINDEX];
     return CHOICE;
 }
 
-//Function that checks player and computer choice and returns a winner
-function playRound(PLAYERSELECTION){
+// Function that checks player and computer choice and returns a winner
+function playRound(PLAYERSELECTION) {
     const COMPUTERSELECTION = getComputerChoice(ARRAY);
-    if (PLAYERSELECTION === COMPUTERSELECTION){ 
+    if (PLAYERSELECTION === COMPUTERSELECTION) {
         results.textContent = `It's a tie! You both picked ${PLAYERSELECTION}. Score: ${playerScore} - ${computerScore}.`;
     } else {
-        if (PLAYERSELECTION === "Rock"){
-            if (COMPUTERSELECTION === "Scissors"){
+        if (PLAYERSELECTION === 'Rock') {
+            if (COMPUTERSELECTION === 'Scissors') {
                 playerScore += 1;
                 results.textContent = `You win! Rock beats Scissors. Score: ${playerScore} - ${computerScore}.`;
             } else {
@@ -48,8 +30,8 @@ function playRound(PLAYERSELECTION){
                 results.textContent = `You lose! Rock loses to Paper. Score: ${playerScore} - ${computerScore}.`;
             }
         }
-        if (PLAYERSELECTION === "Paper"){
-            if (COMPUTERSELECTION === "Rock"){
+        if (PLAYERSELECTION === 'Paper') {
+            if (COMPUTERSELECTION === 'Rock') {
                 playerScore += 1;
                 results.textContent = `You win! Paper beats Rock. Score: ${playerScore} - ${computerScore}.`;
             } else {
@@ -57,8 +39,8 @@ function playRound(PLAYERSELECTION){
                 results.textContent = `You lose! Paper loses to Scissors. Score: ${playerScore} - ${computerScore}.`;
             }
         }
-        if (PLAYERSELECTION === "Scissors"){
-            if (COMPUTERSELECTION === "Paper"){
+        if (PLAYERSELECTION === 'Scissors') {
+            if (COMPUTERSELECTION === 'Paper') {
                 playerScore += 1;
                 results.textContent = `You win! Scissors beat Paper. Score: ${playerScore} - ${computerScore}.`;
             } else {
@@ -69,11 +51,29 @@ function playRound(PLAYERSELECTION){
     }
     if (playerScore === 5 || computerScore === 5) {
         if (playerScore > computerScore) {
-            results.textContent = results.textContent + " Congratulations! You won!";
+            results.textContent += ' Congratulations! You won!';
         } else {
-            results.textContent = results.textContent + " Sorry... You lost...";
+            results.textContent += ' Sorry... You lost...';
         }
         playerScore = 0;
         computerScore = 0;
     }
 }
+
+// Player chooses Rock
+function choiceRock() {
+    playRound('Rock');
+}
+document.querySelector('.rock').addEventListener('click', choiceRock);
+
+// Player chooses Paper
+function choicePaper() {
+    playRound('Paper');
+}
+document.querySelector('.paper').addEventListener('click', choicePaper);
+
+// Player chooses Scissors
+function choiceScissors() {
+    playRound('Scissors');
+}
+document.querySelector('.scissors').addEventListener('click', choiceScissors);
